2e541ebd0b0ad9a96ef5ecd3bd2e8c447eb2e1c3
Tjeu Kayim <15987676+TjeuKayim@users.noreply.github.com>
2020-04-27 17:14:57 +0200

    Include xt_NFQUEUE

--- wrapper.h
+++ wrapper.h
@@ -1,14 +1,4 @@
-// #include <ip4tc.h>
-// #include <ip6tc.h>
-
-// #include "iptables.h" not working
-
-// No IPv6
-// #include "libiptc/libiptc.h"
-// #include "linux/netfilter/xt_limit.h"
-// #include "linux/netfilter/xt_physdev.h"
-// #include <netinet/in.h>
-
 #include "libiptc/libip6tc.h"
 #include "libiptc/libiptc.h"
 #include "linux/netfilter/xt_bpf.h"
+#include "linux/netfilter/xt_NFQUEUE.h"
