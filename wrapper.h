df60d103c7903567f03853b4a7e442b862e8da63
Tjeu Kayim <15987676+TjeuKayim@users.noreply.github.com>
2020-05-19 12:54:19 +0200

    Include IPv6 HL & IPv4 TTL

--- wrapper.h
+++ wrapper.h
@@ -3,3 +3,9 @@
 #include "linux/netfilter/xt_bpf.h"
 #include "linux/netfilter/xt_NFQUEUE.h"
 #include "linux/netfilter/xt_comment.h"
+#include "linux/netfilter_ipv6/ip6t_HL.h"
+#include "linux/netfilter_ipv6/ip6t_hl.h"
+#include "linux/netfilter_ipv4/ipt_TTL.h"
+#include "linux/netfilter_ipv4/ipt_ttl.h"
+
+// /usr/include/linux/netfilter_ipv6/ip6t_HL.h
