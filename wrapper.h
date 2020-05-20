3d2e2589c8f6ed6902cccef24669149729238fe6
Tjeu Kayim <15987676+TjeuKayim@users.noreply.github.com>
2020-05-20 08:50:22 +0200

    Include HL and TTL match and target extensions

--- wrapper.h
+++ wrapper.h
@@ -3,9 +3,11 @@
 #include "linux/netfilter/xt_bpf.h"
 #include "linux/netfilter/xt_NFQUEUE.h"
 #include "linux/netfilter/xt_comment.h"
-#include "linux/netfilter_ipv6/ip6t_HL.h"
 #include "linux/netfilter_ipv6/ip6t_hl.h"
-#include "linux/netfilter_ipv4/ipt_TTL.h"
+#undef _IP6T_HL_H //FIXME: should be a better way
+#include "linux/netfilter_ipv6/ip6t_HL.h"
 #include "linux/netfilter_ipv4/ipt_ttl.h"
+#undef _IPT_TTL_H
+#include "linux/netfilter_ipv4/ipt_TTL.h"
 
 // /usr/include/linux/netfilter_ipv6/ip6t_HL.h
